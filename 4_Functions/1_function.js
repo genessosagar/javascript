e1ee110e2b4bec3a9659a4fbaa697f23e3ec3a82
sagar <davidvidyasagar@outlook.com>
2023-02-23 00:09:38 +0000

    Functions introduction

---
+++ 4_Functions/1_function.js
@@ -0,0 +1,13 @@
+greetWorld();
+
+function greetWorld() {
+    console.log('Hello World');
+}
+
+function getReminder() {
+    console.log('Water the plants.')
+  }
+  
+  function greetInSpanish() {
+    console.log('Buenas tardes.')
+  }
